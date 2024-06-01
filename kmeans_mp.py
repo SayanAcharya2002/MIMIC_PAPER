d003e78dd5e1c1f6cc5f86c218de7738b85a3893
SayanAcharya2002 <sayanacharya8@gmail.com>
2024-06-01 11:43:03 +0530

    Changes_for_mimic_sample

--- kmeans_mp.py
+++ kmeans_mp.py
@@ -14,11 +14,18 @@ def kmeans_with_multiple_runs(ncl,max_iter,nclustering,sampl):
     
     num_processors = os.cpu_count() 
     p=Pool(processes = num_processors)
-          
+    # print(sampl.shape,np.average(np.sum(np.isnan(sampl),axis=1)),np.median(np.sum(np.isnan(sampl),axis=1)))
+    # print(np.where(np.isnan(sampl)))
+    nan_loc=np.where(np.isnan(sampl))
+    sampl[nan_loc[0],nan_loc[1]]=0 # take away the specific axes with nan values
     args = [] 
-        
+    any_nan=False
+    any_inf=False
     for i in range(nclustering): 
         args.append([ncl,max_iter,sampl,i])
+        any_nan|=np.any(np.isnan(sampl))
+        any_inf|=np.any(np.isinf(sampl))
+    print(len(args),any_inf,any_nan)
     clusters = p.starmap(run,args)
 
     inertias = []
